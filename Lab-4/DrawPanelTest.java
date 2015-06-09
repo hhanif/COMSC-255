b7a4d265f786d2f919ada213171b8c1b13984ed5
Haris Hanif <hhanif@users.noreply.github.com>
2015-06-08 17:30:33 -0700

    Create DrawPanelTest.java

---
+++ Lab-4/DrawPanelTest.java
@@ -0,0 +1,22 @@
+// File: DrawPanelTest.java
+// Name: Haris Hanif
+// DVC ID: 1392603
+// IDE:  Eclipse Luna
+import javax.swing.JFrame;
+public class DrawPanelTest {
+	
+	public static void main( String[] args ) {
+		// create a panel that contains our drawing
+		DrawPanel panel = new DrawPanel();
+
+		// create a new frame to hold the panel
+		JFrame application = new JFrame();
+
+		// set the frame to exit when it is closed
+		application.setDefaultCloseOperation( JFrame. EXIT_ON_CLOSE );
+
+		application.add( panel ); // add the panel to the frame
+		application.setSize( 250, 250 ); // set the size of the frame
+		application.setVisible( true ); // make the frame visible
+ } // end main
+ } // end class DrawPanelTest
