5dfa6837aa6b20eff576b88465a1cb8ac2235065
Haris Hanif <hhanif@users.noreply.github.com>
2015-06-08 23:46:04 -0700

    Create ShapeTest.java

---
+++ Assignment-8/ShapeTest.java
@@ -0,0 +1,20 @@
+// File: ShapeTest.java
+// Name: Haris Hanif 
+// DVC ID: 1392603
+// IDE:  Eclipse Luna
+public class ShapeTest {
+	public static void main(String[] args) {
+
+	Circle a = new Circle(1.0, "red", true);
+	System.out.print(a.toString());
+	System.out.println();
+	Rectangle b = new Rectangle(33.3, 22.2, "green", true);
+	System.out.print(b.toString());
+	System.out.println();
+
+	Square c = new Square(11.1, "red", true);
+	System.out.print(c.toString());
+
+	}
+
+}
