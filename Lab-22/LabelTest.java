cf228c313bedcd17649fe66f5d69ed7ee1fdc028
Haris Hanif <hhanif@users.noreply.github.com>
2015-06-08 19:34:38 -0700

    Create LabelTest.java

---
+++ Lab-22/LabelTest.java
@@ -0,0 +1,18 @@
+// File: LabelTest.java
+// Name: Haris Hanif
+// DVC ID: 1392603
+// IDE:  Eclipse Luna
+// Fig. 14.7: LabelTest.java
+// Testing LabelFrame.
+import javax.swing.JFrame;
+
+public class LabelTest 
+{
+   public static void main( String[] args )
+   { 
+      LabelFrame labelFrame = new LabelFrame(); // create LabelFrame
+      labelFrame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+      labelFrame.setSize( 260, 180 ); // set frame size
+      labelFrame.setVisible( true ); // display frame
+   } // end main
+} // end class LabelTest
