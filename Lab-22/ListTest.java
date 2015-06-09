128f503d9ad3fb484ff8e9d1dfb8dcad3381094e
Haris Hanif <hhanif@users.noreply.github.com>
2015-06-08 19:32:56 -0700

    Create ListTest.java

---
+++ Lab-22/ListTest.java
@@ -0,0 +1,18 @@
+// File: ListTest.java
+// Name: Haris Hanif
+// DVC ID: 1392603
+// IDE:  Eclipse Luna
+// Fig. 14.24: ListTest.java
+// Selecting colors from a JList.
+import javax.swing.JFrame;
+
+public class ListTest 
+{
+   public static void main( String[] args )
+   { 
+      ListFrame listFrame = new ListFrame(); // create ListFrame
+      listFrame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+      listFrame.setSize( 350, 150 ); // set frame size
+      listFrame.setVisible( true ); // display frame
+   } // end main
+} // end class ListTest
