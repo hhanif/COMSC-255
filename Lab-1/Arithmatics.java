531a387d512b4fea436895af55446628e8368d32
Haris Hanif <hhanif@users.noreply.github.com>
2015-06-08 17:13:36 -0700

    Create Arithmatics.java

---
+++ Lab-1/Arithmatics.java
@@ -0,0 +1,34 @@
+// File: Arithmatics.java
+// Name: Haris Hanif
+// DVC ID: 1392603
+// IDE:  Eclipse Luna
+// Fig. 3.13: Account. java
+// Account class with a constructor to validate and
+// initialize instance variable balance of type double.
+
+public class Arithmatics {
+	public static void main( String[] args )
+	{
+	}
+	 private double balance; // instance variable that stores the balance 8
+	 // constructor
+	 public Arithmatics(double initialBalance)
+	 {
+		 // validate that initialBalance is greater than 0.0;
+		// if it is not, balance is initialized to the default value 0.0
+		if ( initialBalance > 0.0 )
+		balance = initialBalance; 
+	} // end Account constructor
+
+	//credit (add) an amount to the account
+	 public void credit( double amount )
+	 {
+		 balance = balance + amount; // add amount to balance
+	 } //end method credit
+	 
+	//return the account balance
+	 public double getBalance()
+	 {
+		 return balance; // gives the value of balance to the calling method
+	 } // end method getBalance
+} // end class Account
