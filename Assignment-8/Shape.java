8f19c574a3262cef972c536981602407477b1590
Haris Hanif <hhanif@users.noreply.github.com>
2015-06-08 23:45:50 -0700

    Create Shape.java

---
+++ Assignment-8/Shape.java
@@ -0,0 +1,43 @@
+// File: Shape.java
+// Name: Haris Hanif 
+// DVC ID: 1392603
+// IDE:  Eclipse Luna
+public class Shape {
+	private String color;
+	private boolean filled;
+	
+	public Shape() {
+		this("green", true);
+	}
+	public Shape(String c, boolean f) {
+		setColor(c);
+		setFilled(f);
+	}
+	
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public boolean isFilled() {
+		return filled;
+	}
+
+	public void setFilled(boolean filled) {
+		this.filled = filled;
+	}
+	
+	public String toString() {
+		if(isFilled() == true) {
+			return "A shape with color of " + this.getColor() + " is filled";	
+		}
+		else {
+			return "A shape with color of " + this.getColor() + " is not filled";
+		}
+	}
+	
+}
