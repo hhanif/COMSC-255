873496b8bfd524b0f62032670aa11a356fa10efb
Haris Hanif <hhanif@users.noreply.github.com>
2015-06-08 23:54:47 -0700

    Create Movable.java

---
+++ Assignment-10/Movable.java
@@ -0,0 +1,10 @@
+// File: Movable.java
+// Name: Haris Hanif
+// DVC ID: 1392603
+// IDE:  Eclipse Luna
+public interface Movable {
+	public void moveUp();
+	public void moveDown();
+	public void moveLeft();
+	public void moveRight();
+}
