614164ec52937b0a8573de24fb6e21551fbdbc50
Haris Hanif <hhanif@users.noreply.github.com>
2015-06-08 17:13:13 -0700

    Create Welcome.java

---
+++ Lab-1/Welcome.java
@@ -0,0 +1,12 @@
+// File: Welcome.java
+// Name: Haris Hanif
+// DVC ID: 1392603
+// IDE:  Eclipse Luna
+public class Welcome {
+	public static void main( String[] args ) {
+    System.out.printf("My name is Haris Hanif.\n");
+    System.out.printf("This is my 4th semester at DVC.\n");
+    System.out.printf("My major is Computer Science.\n");
+    System.out.printf("I also know C++ programming and HTML.\n");
+	}
+}
