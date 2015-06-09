9d8ce49867ed8ba9b92d2c644b29757e6a73c946
Haris Hanif <hhanif@users.noreply.github.com>
2015-06-08 18:49:46 -0700

    Create GeometricObjects.java

---
+++ Lab-19/GeometricObjects.java
@@ -0,0 +1,9 @@
+// File: GeometricObjects.java
+// Name: Haris Hanif
+// DVC ID: 1392603
+// IDE:  Eclipse Luna
+public interface GeometricObjects {
+	public double getArea();
+	public double getPerimeter();
+
+}
