6db9359154fcf70a3f20f32ea0e15382c385466f
Haris Hanif <hhanif@users.noreply.github.com>
2015-06-08 19:21:29 -0700

    Create TextAreaDemo.java

---
+++ Lab-22/TextAreaDemo.java
@@ -0,0 +1,18 @@
+// File: TextAreaDemo.java
+// Name: Haris Hanif
+// DVC ID: 1392603
+// IDE:  Eclipse Luna
+// Fig. 14.48: TextAreaDemo.java
+// Copying selected text from one textarea to another. 
+import javax.swing.JFrame;
+
+public class TextAreaDemo
+{
+   public static void main( String[] args )
+   { 
+      TextAreaFrame textAreaFrame = new TextAreaFrame(); 
+      textAreaFrame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+      textAreaFrame.setSize( 425, 200 ); // set frame size
+      textAreaFrame.setVisible( true ); // display frame
+   } // end main
+} // end class TextAreaDemo
