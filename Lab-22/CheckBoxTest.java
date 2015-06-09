12d0e2b4205484b2eb13af1bc520ed0e586254d7
Haris Hanif <hhanif@users.noreply.github.com>
2015-06-08 19:39:05 -0700

    Create CheckBoxTest.java

---
+++ Lab-22/CheckBoxTest.java
@@ -0,0 +1,18 @@
+// File: CheckBoxTest.java
+// Name: Haris Hanif
+// DVC ID: 1392603
+// IDE:  Eclipse Luna
+// Fig. 14.18: CheckBoxTest.java
+// Testing CheckBoxFrame.
+import javax.swing.JFrame;
+
+public class CheckBoxTest
+{
+   public static void main( String[] args )
+   { 
+      CheckBoxFrame checkBoxFrame = new CheckBoxFrame(); 
+      checkBoxFrame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+      checkBoxFrame.setSize( 275, 100 ); // set frame size
+      checkBoxFrame.setVisible( true ); // display frame
+   } // end main
+} // end class CheckBoxTest
