3c39456d51a4b0e37abf63b237ee507ae60008e3
Haris Hanif <hhanif@users.noreply.github.com>
2015-06-08 19:38:16 -0700

    Create FlowLayoutDemo.java

---
+++ Lab-22/FlowLayoutDemo.java
@@ -0,0 +1,18 @@
+// File: FlowLayoutDemo.java
+// Name: Haris Hanif
+// DVC ID: 1392603
+// IDE:  Eclipse Luna
+// Fig. 14.40: FlowLayoutDemo.java
+// Testing FlowLayoutFrame.
+import javax.swing.JFrame;
+
+public class FlowLayoutDemo 
+{
+   public static void main( String[] args )
+   { 
+      FlowLayoutFrame flowLayoutFrame = new FlowLayoutFrame(); 
+      flowLayoutFrame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+      flowLayoutFrame.setSize( 300, 75 ); // set frame size
+      flowLayoutFrame.setVisible( true ); // display frame
+   } // end main
+} // end class FlowLayoutDemo
