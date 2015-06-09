b79f57b6781c3df199214eb0b17d41056c541b0d
Haris Hanif <hhanif@users.noreply.github.com>
2015-06-08 18:54:02 -0700

    Create Payable.java

---
+++ Lab-20/Payable.java
@@ -0,0 +1,8 @@
+// File: Payable.java
+// Name: Haris Hanif
+// DVC ID: 1392603
+// IDE:  Eclipse Luna
+public interface Payable
+{
+double getPaymentAmount(); // calculate payment; no implementation
+} // end interface Payable
