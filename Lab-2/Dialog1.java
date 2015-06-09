ffb7dfb09f11390854ef101d63a0db8400f3e36d
Haris Hanif <hhanif@users.noreply.github.com>
2015-06-08 17:22:48 -0700

    Create Dialog1.java

---
+++ Lab-2/Dialog1.java
@@ -0,0 +1,16 @@
+// File: Dialog1.java
+// Name: Haris Hanif
+// DVC ID: 1392603
+// IDE:  Eclipse Luna
+// Fig. 3.17: Dialogl1.java
+// Using DOptionPane to display multiple lines in a dialog box.
+import javax.swing.JOptionPane; // import class DOptionPane
+public class Dialog1 {
+
+	public static void main( String[] args )
+	{
+	// display a dialog with a message
+	JOptionPane.showMessageDialog( null, "My name is Haris Hanif.\nThis is my 4th semester at DVC.\nMy major is computer science.\nI also know C++ programming and HTML." ); 
+	} // end main
+
+}
